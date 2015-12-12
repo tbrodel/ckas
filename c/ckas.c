605d6485a5efa4cbafa906ed091ea6dbcd90335a
Tobias Brodel <toby@cataz.bubsr.lan>
2015-12-12 13:51:27 +1100

    whitespace fix

--- c/ckas.c
+++ c/ckas.c
@@ -29,7 +29,7 @@ typedef struct t_ckas {
         t_object x_obj;
         t_float x, y, z, pitch, roll, yaw;
         char *host; /* String to hold CKAS target IP */
-		int sockfd; /* Socket file descriptor */
+    	int sockfd; /* Socket file descriptor */
 } t_ckas;
 
 void
@@ -50,7 +50,7 @@ send_packet(t_ckas *x)
         snprintf(packet, (PACKET_LEN + NULL_TERM) * sizeof(char), 
             "%s%010.6f, %010.6f, %010.6f, %09.6f, %09.6f, %09.6f", HEAD, x->x,
             x->y, x->z, x->pitch, x->roll, x->yaw);
-		packet[PACKET_LEN] = '\0';
+    	packet[PACKET_LEN] = '\0';
 
         if (sendto(x->sockfd, packet, PACKET_LEN + NULL_TERM, 0, 
             (struct sockaddr *)&simcor, slen) == -1) {
@@ -66,7 +66,7 @@ mk_sockfd(t_ckas *x)
         if ((x->sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1) {
                 perror("fail..");
                 post("Failed to open socket");
-		}
+    	}
 }
 
 void
@@ -84,7 +84,7 @@ check_float(t_float *f, float max, float min)
 void
 ckas_bang(t_ckas *x)
 {
-	/* Ensure values are in valid range */
+    /* Ensure values are in valid range */
     check_float(&x->x, CART_MAX, CART_MIN);
     check_float(&x->y, CART_MAX, CART_MIN);
     check_float(&x->z, CART_MAX, CART_MIN);
@@ -108,10 +108,10 @@ ckas_new(t_symbol *s, int argc, t_atom *argv)
         floatinlet_new(&x->x_obj, &x->roll);
         floatinlet_new(&x->x_obj, &x->yaw);
         
-		/* 
+    	/* 
          * If object has an argument make it the IP address, otherwise default
-		 * to localhost.
-		 */
+    	 * to localhost.
+    	 */
         switch(argc) {
         case 1:
                 sym = atom_getsymbolarg(0, argc, argv);
@@ -122,8 +122,8 @@ ckas_new(t_symbol *s, int argc, t_atom *argv)
                 x->host = LOCAL_IP;
         }
 
-		/* Make socket descriptor once, changing sockets confuses Simcor */
-		mk_sockfd(x);
+    	/* Make socket descriptor once, changing sockets confuses Simcor */
+    	mk_sockfd(x);
 
         return (void *)x;
 }
@@ -131,15 +131,15 @@ ckas_new(t_symbol *s, int argc, t_atom *argv)
 void 
 ckas_free(t_ckas *x)
 {
-		/* Close socket when object is destroyed */
-		close(x->sockfd);
+    	/* Close socket when object is destroyed */
+    	close(x->sockfd);
 }
 
 void
 ckas_setup(void)
 {
         ckas_class = class_new(gensym("ckas"), (t_newmethod)ckas_new, 
-			(t_method)ckas_free, sizeof(t_ckas), CLASS_DEFAULT, A_GIMME, 0); 
+    		(t_method)ckas_free, sizeof(t_ckas), CLASS_DEFAULT, A_GIMME, 0); 
 
         class_addbang(ckas_class, ckas_bang);
 }
